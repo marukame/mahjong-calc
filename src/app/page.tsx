e4bfa2c18f351b35e4bb42736f39b07df4003c6a
r-kameda02 <r_kameda@waku-2.com>
2025-07-09 23:22:10 +0900

    準備

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MahjongCalculator from '@/components/MahjongCalculator'
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <MahjongCalculator />
+    </main>
+  )
+}
